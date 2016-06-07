aeda8ccc80be08aef5108dee0f4894be04b1b4c6
Дмитрий Басавин <basavind@gmail.com>
2016-06-07 12:56:00 +0300

    fix (Navigation) upload material route

--- templates/part.navigation.php
+++ templates/part.navigation.php
@@ -1,4 +1,4 @@
 <ul>
     <li><a href="/apps/cbreeder/section">Направления</a></li>
-    <li><a href="/apps/cbreeder/load">Загрузить материал</a></li>
+    <li><a href="/apps/cbreeder/material">Загрузить материал</a></li>
 </ul>
